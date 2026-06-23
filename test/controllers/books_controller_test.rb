24c835e14ec433cb1e9ba02e6ce7b92c3c385764
JunichiHattori <junhat6@gmail.com>
2026-06-23 18:54:35 +0900

    記事詳細画面を追加

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -8,4 +8,10 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     get books_url
     assert_response :success
   end
+
+  test "should get show" do
+    book = books(:one)
+    get book_url(book)
+    assert_response :success
+  end
 end
