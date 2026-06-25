5c3b888a4ba90ca6ce0887a4f45d9660536728f2
JunichiHattori <junhat6@gmail.com>
2026-06-25 22:30:10 +0900

    本の作成機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :books, only: [ :index, :show ]
+  resources :books, only: [ :index, :show, :new, :create ]
   root "books#index"
 end
