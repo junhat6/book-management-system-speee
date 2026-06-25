7aa9584f06cd091484cc129f05be7e1beb0919e9
JunichiHattori <junhat6@gmail.com>
2026-06-25 22:55:03 +0900

    faker annotaterb bullet 追加 - faker はそれっぽいテストデータ文字列作成 - annotaterbはmodelにスキーマ情報付与 - bullet はN+1問題を検知 jsコンソール、railsログ、jsフッター

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,5 +1,21 @@
 require "test_helper"
 
+# == Schema Information
+#
+# Table name: books
+#
+#  id             :integer          not null, primary key
+#  isbn           :string
+#  published_year :integer
+#  publisher      :string
+#  title          :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_books_on_isbn  (isbn) UNIQUE
+#
 class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
